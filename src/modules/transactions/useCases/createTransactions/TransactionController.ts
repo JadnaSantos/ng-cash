98e4cc0cf0ebdd2fa3e7defafb3bca5fff548ac1
Jadna Santos <jadnajesus7@gmail.com>
2023-01-26 14:40:43 -0300

    new transaction useCase

---
+++ src/modules/transactions/useCases/createTransactions/TransactionController.ts
@@ -0,0 +1,10 @@
+import { Transactions } from '@prisma/client';
+import { IAccountRepository } from '../../../account/repositories/interface/IAccountRepository';
+import { ITransactionRepository } from "../../repositories/interface/ITransactionRepository";
+
+
+class TransactionController {
+
+}
+
+export { TransactionController };
---
+++ src/modules/transactions/useCases/createTransactions/TransactionUseCase.ts
@@ -0,0 +1,24 @@
+import { Transactions } from '@prisma/client';
+import { inject, injectable } from 'tsyringe';
+import { IAccountRepository } from '../../../account/repositories/interface/IAccountRepository';
+import { ITransactionDTO } from '../../dtos/ITransactionDTO';
+import { ITransactionRepository } from '../../repositories/interface/ITransactionRepository';
+
+interface IRequest {
+  debited_user: string,
+  credited_user: string,
+  value: number,
+}
+
+@injectable()
+class TransactionUseCase {
+  constructor(
+    @inject('TransactionRepository')
+    private transactionRepository: ITransactionRepository,
+    @inject('AccountUserRepository')
+    private accountRepository: IAccountRepository
+  ) { }
+
+}
+
+export { TransactionUseCase };
